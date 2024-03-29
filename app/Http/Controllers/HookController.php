8e364b1e3696466cfcc4eac9666334802af49b28
Muxammadali1998 <turgunaliyev.98@gmail.com>
2024-03-29 14:20:56 +0500

    optimization

--- app/Http/Controllers/HookController.php
+++ app/Http/Controllers/HookController.php
@@ -13,9 +13,9 @@ class HookController extends Controller
     {
         try {
             if (isset($request->contacts)) {
-                $this->contact($request->contacts);
+                $this->contact($request->contacts, $request->account);
             } elseif (isset($request->leads)) {
-                $this->lead($request->leads);
+                $this->lead($request->leads, $request->account);
             }
             return response()->json(['message' => 'Webhook processed successfully']);
         }
@@ -25,34 +25,35 @@ class HookController extends Controller
 
     }
 
-    public function contact($contacts): void
+    public function contact($contacts, $account): void
     {
         if (isset($contacts['add'])) {
-            $this->saveContact($contacts['add'], $contacts['account']['subdomain']??'');
+            $this->saveContact($contacts['add'], $account['subdomain']??'');
         } elseif (isset($contacts['update'])) {
-            $this->saveContact($contacts['update'], $contacts['account']['subdomain']??'');
-            $this->saveUpdatedContact($contacts['update'], $contacts['account']['subdomain']??'');
+            $this->saveContact($contacts['update'], $account['subdomain']??'');
+            $this->saveUpdatedContact($contacts['update'], $account['subdomain']??'');
         }
     }
 
-    public function lead($leads): void
+    public function lead($leads, $account): void
     {
         if (isset($leads['add'])) {
-            $this->saveLead($leads['add'], $leads['account']['subdomain']??'');
+            $this->saveLead($leads['add'], $account['subdomain']??'');
         } elseif (isset($leads['update'])) {
-            $this->saveLead($leads['update'], $leads['account']['subdomain']??'');
-            $this->saveUpdatedLead($leads['update'], $leads['account']['subdomain']??'');
+            $this->saveLead($leads['update'], $account['subdomain']??'');
+            $this->saveUpdatedLead($leads['update'], $account['subdomain']??'');
         }
     }
 
     public function saveContact($contacts, $user): void
     {
         foreach ($contacts as $contact) {
+
             $contact['created_at'] = Carbon::createFromTimestamp($contact['created_at']);
             $contact['updated_at'] = Carbon::createFromTimestamp($contact['updated_at']);
             $contact[ 'responsible_user'] = $user;
-            Contact::create($contact);
 
+            Contact::updateOrCreate(['id'=>$contact['id']],$contact);
         }
     }
 
@@ -67,25 +68,25 @@ class HookController extends Controller
         }
     }
 
-    public function saveLead($contacts, $user): void
+    public function saveLead($leads, $user): void
     {
-        foreach ($contacts as $contact) {
-            $contact['created_at'] = Carbon::createFromTimestamp($contact['created_at']);
-            $contact['updated_at'] = Carbon::createFromTimestamp($contact['updated_at']);
-            $contact[ 'responsible_user'] = $user;
-            Lead::create($contact);
+        foreach ($leads as $lead) {
+            $lead['created_at'] = Carbon::createFromTimestamp($lead['created_at']);
+            $lead['updated_at'] = Carbon::createFromTimestamp($lead['updated_at']);
+            $lead[ 'responsible_user'] = $user;
+            Lead::updateOrCreate(['id'=>$lead['id']],$lead);
 
         }
     }
 
-    public function saveUpdatedLead($contacts, $user): void
+    public function saveUpdatedLead($leads, $user): void
     {
-        foreach ($contacts as $contact) {
-            $contact['created_at'] = Carbon::createFromTimestamp($contact['created_at']);
-            $contact['updated_at'] = Carbon::createFromTimestamp($contact['updated_at']);
-            $contact['contact_id'] = Carbon::createFromTimestamp($contact['updated_at']);
-            $contact[ 'responsible_user'] = $user;
-            Lead::create( $contact);
+        foreach ($leads as $lead) {
+            $lead['created_at'] = Carbon::createFromTimestamp($lead['created_at']);
+            $lead['updated_at'] = Carbon::createFromTimestamp($lead['updated_at']);
+            $lead['contact_id'] = Carbon::createFromTimestamp($lead['updated_at']);
+            $lead[ 'responsible_user'] = $user;
+            Lead::create( $lead);
         }
     }
 
